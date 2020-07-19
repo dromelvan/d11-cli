05c877a6ca32b4f65fa69a3290247179357fcf83
dromelvan <dromelvan@aland.net>
2020-07-19 23:48:50 +0300

    Print usage information on top level d11 command.

--- src/main/java/org/d11/cli/D11Command.java
+++ src/main/java/org/d11/cli/D11Command.java
@@ -4,6 +4,7 @@ import java.util.concurrent.Callable;
 
 import org.springframework.stereotype.Component;
 
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 @Component
@@ -16,7 +17,8 @@ public class D11Command implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        System.out.println("Call D11");
+        CommandLine commandLine = new CommandLine(this);
+        commandLine.usage(System.out);
         return 0;
     }
     
